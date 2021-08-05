063eb8a53380ffe4a86fe0637a0a3b30fbd24367
Ralf Lang <lang@b1-systems.de>
2021-08-05 16:14:47 +0000

    Allow PHP8, move Unit Tests to own namespace

--- test/CombineTest.php
+++ test/CombineTest.php
@@ -1,70 +1,54 @@
 <?php
 /**
- * Copyright 2008-2017 Horde LLC (http://www.horde.org/)
+ * Copyright 2009-2017 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (BSD). If you
  * did not receive this file, see http://www.horde.org/licenses/bsd.
  *
  * @category   Horde
- * @copyright  2008-2016 Horde LLC
+ * @copyright  2009-2016 Horde LLC
  * @license    http://www.horde.org/licenses/bsd BSD
  * @package    Stream_Wrapper
  * @subpackage UnitTests
  */
-namespace Horde\Stream;
-use \Wrapper;
+namespace Horde\Stream\Wrapper\Test;
 use PHPUnit\Framework\TestCase;
-use Horde_Stream_Wrapper_String;
+use Horde_Stream_Wrapper_Combine;
 
 /**
- * Tests for the String wrapper.
+ * Tests for the Combine wrapper.
  *
  * @author     Michael Slusarz <slusarz@horde.org>
  * @category   Horde
- * @copyright  2008-2016 Horde LLC
+ * @copyright  2009-2016 Horde LLC
  * @ignore
  * @license    http://www.horde.org/licenses/bsd BSD
  * @package    Stream_Wrapper
  * @subpackage UnitTests
  */
-class StringTest extends TestCase
+class CombineTest extends TestCase
 {
     public function testUsage()
     {
-        $string = 'ABCDE12345fghij';
+        $fp = fopen('php://temp', 'r+');
+        fwrite($fp, '12345');
 
-        $stream = Horde_Stream_Wrapper_String::getStream($string);
+        $data = array('ABCDE', $fp, 'fghij');
+
+        $stream = Horde_Stream_Wrapper_Combine::getStream($data);
 
         $this->assertEquals('ABCDE12345fghij', fread($stream, 1024));
         $this->assertEquals(true, feof($stream));
         $this->assertEquals(0, fseek($stream, 0));
-        $this->assertEquals(0, fseek($stream, 0));
+        $this->assertEquals(-1, fseek($stream, 0));
         $this->assertEquals(0, ftell($stream));
         $this->assertEquals(0, fseek($stream, 5, SEEK_CUR));
         $this->assertEquals(5, ftell($stream));
         $this->assertEquals(10, fwrite($stream, '0000000000'));
         $this->assertEquals(0, fseek($stream, 0, SEEK_END));
-        $this->assertEquals(15, ftell($stream));
+        $this->assertEquals(20, ftell($stream));
         $this->assertEquals(false, feof($stream));
 
         fclose($stream);
     }
-
-    public function testMemoryUsage()
-    {
-        $bytes = 1024 * 1024;
-        $string = str_repeat('*', $bytes);
-        $memoryUsage = memory_get_usage();
-
-        $stream = Horde_Stream_Wrapper_String::getStream($string);
-        $memoryUsage2 = memory_get_usage();
-        $this->assertLessThan($memoryUsage + $bytes, $memoryUsage2);
-
-        while (!feof($stream)) {
-            fread($stream, 1024);
-        }
-        $memoryUsage3 = memory_get_usage();
-        $this->assertLessThan($memoryUsage + $bytes, $memoryUsage3);
-    }
-
 }
--- test/StringTest.php
+++ test/StringTest.php
@@ -1,55 +1,69 @@
 <?php
 /**
- * Copyright 2009-2017 Horde LLC (http://www.horde.org/)
+ * Copyright 2008-2017 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (BSD). If you
  * did not receive this file, see http://www.horde.org/licenses/bsd.
  *
  * @category   Horde
- * @copyright  2009-2016 Horde LLC
+ * @copyright  2008-2016 Horde LLC
  * @license    http://www.horde.org/licenses/bsd BSD
  * @package    Stream_Wrapper
  * @subpackage UnitTests
  */
-namespace Horde\Stream;
-use \Wrapper;
+namespace Horde\Stream\Wrapper\Test;
 use PHPUnit\Framework\TestCase;
-use Horde_Stream_Wrapper_Combine;
+use Horde_Stream_Wrapper_String;
 
 /**
- * Tests for the Combine wrapper.
+ * Tests for the String wrapper.
  *
  * @author     Michael Slusarz <slusarz@horde.org>
  * @category   Horde
- * @copyright  2009-2016 Horde LLC
+ * @copyright  2008-2016 Horde LLC
  * @ignore
  * @license    http://www.horde.org/licenses/bsd BSD
  * @package    Stream_Wrapper
  * @subpackage UnitTests
  */
-class CombineTest extends TestCase
+class StringTest extends TestCase
 {
     public function testUsage()
     {
-        $fp = fopen('php://temp', 'r+');
-        fwrite($fp, '12345');
+        $string = 'ABCDE12345fghij';
 
-        $data = array('ABCDE', $fp, 'fghij');
-
-        $stream = Horde_Stream_Wrapper_Combine::getStream($data);
+        $stream = Horde_Stream_Wrapper_String::getStream($string);
 
         $this->assertEquals('ABCDE12345fghij', fread($stream, 1024));
         $this->assertEquals(true, feof($stream));
         $this->assertEquals(0, fseek($stream, 0));
-        $this->assertEquals(-1, fseek($stream, 0));
+        $this->assertEquals(0, fseek($stream, 0));
         $this->assertEquals(0, ftell($stream));
         $this->assertEquals(0, fseek($stream, 5, SEEK_CUR));
         $this->assertEquals(5, ftell($stream));
         $this->assertEquals(10, fwrite($stream, '0000000000'));
         $this->assertEquals(0, fseek($stream, 0, SEEK_END));
-        $this->assertEquals(20, ftell($stream));
+        $this->assertEquals(15, ftell($stream));
         $this->assertEquals(false, feof($stream));
 
         fclose($stream);
     }
+
+    public function testMemoryUsage()
+    {
+        $bytes = 1024 * 1024;
+        $string = str_repeat('*', $bytes);
+        $memoryUsage = memory_get_usage();
+
+        $stream = Horde_Stream_Wrapper_String::getStream($string);
+        $memoryUsage2 = memory_get_usage();
+        $this->assertLessThan($memoryUsage + $bytes, $memoryUsage2);
+
+        while (!feof($stream)) {
+            fread($stream, 1024);
+        }
+        $memoryUsage3 = memory_get_usage();
+        $this->assertLessThan($memoryUsage + $bytes, $memoryUsage3);
+    }
+
 }
